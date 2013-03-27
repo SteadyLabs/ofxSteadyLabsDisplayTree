fd16ae1cedba960d7880361f22d22cf0557b5e6a
bruno <brunokruse@gmail.com>
2013-03-27 14:32:15 -0400

    script to download addons

---
+++ clone_addons.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# addons for display tree
+
+cd ../
+
+if [ -z $1 ]; then
+    PREFIX="git clone https://github.com/"
+else
+    PREFIX="git clone git@github.com:"
+fi
+
+${PREFIX}YCAMInterlab/ofxTimeline.git
